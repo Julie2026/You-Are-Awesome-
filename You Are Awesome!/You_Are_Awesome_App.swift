299312911281b521762835ac71142ab9944d8008
JulieVera2026 <julieverabt21@gmail.com>
2025-12-01 12:35:19 -0800

    Initial Commit

---
+++ You Are Awesome!/You_Are_Awesome_App.swift
@@ -0,0 +1,17 @@
+//
+//  You_Are_Awesome_App.swift
+//  You Are Awesome!
+//
+//  Created by VERA, JULIE on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct You_Are_Awesome_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
